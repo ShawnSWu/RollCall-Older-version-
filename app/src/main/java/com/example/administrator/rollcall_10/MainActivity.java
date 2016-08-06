ebe21d311b432018efad4460bac2b17daa968bed
Shawn Wu <Shawn.S.Wu.newegg.com>
2016-08-06 12:51:12 +0800

    It's begin 1.0

---
+++ app/src/main/java/com/example/administrator/rollcall_10/MainActivity.java
@@ -0,0 +1,130 @@
+package com.example.administrator.rollcall_10;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v7.app.ActionBarDrawerToggle;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener  {
+
+
+    //Toolbar
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //**Toolbar"三"線的變化
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        //**Toolbar"三"線的變化
+
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.main_fragment, new mainview_fragmentlayout_Home())
+                .commitAllowingStateLoss();
+    }
+
+
+    //**Toolbar箭頭的點擊事件
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        //home
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    //**Toolbar箭頭的點擊事件
+
+
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+
+
+
+
+    //**Toolbar其他元件點擊事件
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_drawerlist, menu);
+        return true;
+    }
+    //**Toolbar其他元件點擊事件
+
+
+
+
+
+
+    //**左選單項目點擊事件
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        Fragment mainview = null;
+
+        switch (id){
+            case R.id.Home:
+                mainview=new mainview_fragmentlayout_Home();
+                break;
+
+            case R.id.Set_People:
+                mainview=new mainview_fragmentlayout_SetPeople();
+                break;
+
+            case R.id.Edit_List:
+                mainview=new mainview_fragmentlayout_EditList();
+                break;
+        }
+
+
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.main_fragment, mainview)
+                .commitAllowingStateLoss();
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+    //**左選單項目點擊事件
+
+
+
+}
---
+++ app/src/main/java/com/example/administrator/rollcall_10/mainview_fragmentlayout_Home.java
@@ -0,0 +1,33 @@
+package com.example.administrator.rollcall_10;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2016/8/6.
+ */
+public class mainview_fragmentlayout_Home extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View frist_view=inflater.inflate(R.layout.mainview_fragmentlayout_home,null);
+        return frist_view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
+
