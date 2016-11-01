6c55e50a6b7c8cb6ed834538bcce54edb106d717
shawntest <a5456434@gmail.com>
2016-11-01 22:35:11 +0800

    shawn test

--- app/src/main/java/com/example/administrator/rollcall_10/rollcall_dialog/RollCall_Dialog.java
+++ app/src/main/java/com/example/administrator/rollcall_10/rollcall_dialog/RollCall_Dialog.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 
+
+//shawn test201611/01
 public class RollCall_Dialog extends AlertDialog {
     public RollCall_Dialog(Context context){
         super(context);
